5192160277b1cd47eb37bf8331ce146bcbbb5ec5
trainee1 <erickakegil@outlook.es>
2019-11-12 18:01:47 -0600

    correction v3
    correction v4
    
    correction v5
    
    correction v6

--- dates.rb
+++ dates.rb
@@ -1,38 +1,21 @@
 require 'date'
 
 def dates(first_date, final_date, day)
-  
   dates = []
   first_mon = first_date.mon 
   final_mon = final_date.mon
   final_month = first_mon + final_mon - 1 
   first_year = first_date.year
   final_year = final_date.year
-  final_years = (final_year - first_year)
+  final_years = final_year - first_year
   
-  if final_years == 0 
-    (first_mon..final_mon).each do |numero1|
-      dates.push(Date.new(first_year, numero1, day)) 
-    end   
+return [] unless final_years.zero?
+  (first_mon..final_mon).each do |month|
+    dates.push(Date.new(first_year, month, day)) 
   end
-  
-  return dates
+
+dates
 
 end
 
-dates(Date.new(2017,01,2), Date.new(2017,5,8), 4)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+dates(Date.new(2017, 01, 2), Date.new(2017, 5, 8), 4)
