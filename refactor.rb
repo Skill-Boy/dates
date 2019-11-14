520b1fb231efcc911661f5044116baab848cfe63
trainee1 <erickakegil@outlook.es>
2019-11-14 16:08:37 -0600

    guard_clase_fullname
    guard_clase_fullname v2
    
    guard_clase_fullname v3
    
    guard_clase_fullname v4
    
    guard_clase_fullname v5
    
    guard_clase_fullname v6
    
    guard_clase_fullname v7

--- refactor.rb
+++ refactor.rb
@@ -1,18 +1,21 @@
-
 def customer_state(session_id = nil)
-
   return { state: 'chat_disabled' } unless Setting.get('chat')
-  return unless session_id
-      
+  return session_id   
     chat_session = Chat::Session.find_by(session_id: session_id, state: %w[waiting running])
-      user = nil if chat_session.state == 'running'
-      chat_user = User.lookup(id: chat_session.user_id)
-      running_session(chat_session, session_id)
+      case chat_session.state 
+      when 'running'
+        user = nil
+        chat_user = User.lookup(id: chat_session.user_id)
+        running_session(chat_user, chat_session, session_id)
+      when 'waiting'
+        waiting_session(chat_session)
+      else 
+        "state no found"              
+      end        
        
-  return waiting_session(chat_session) if chat_session.state == 'waiting'
   return { state: 'offline' } if Chat.active_agent_count([id]).zero?
   
-    waiting_count = Chat.waiting_chat_count(id)
+  waiting_count = Chat.waiting_chat_count(id)
   return { state: 'no_seats_available', queue: waiting_count,} if waiting_count >= max_queue
     { state: 'online' }
 end
@@ -21,8 +24,7 @@ def waiting_session(chat_session)
   { state:    'reconnect', position: chat_session.position,}
 end
     
-def running_session(chat_session, session_id)
-
+def running_session(chat_user, chat_session, session_id)
   url = nil
   url = "#{Setting.get('http_type')}://#{Setting.get('fqdn')}/api/v1/users/image/#{chat_user.image}"
     
