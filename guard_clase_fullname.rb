08dc3433ecbbd46ddda4625cbb1689e3b7876f5b
trainee1 <erickakegil@outlook.es>
2019-11-19 11:19:23 -0600

    user_poo
    user_poo v2
    
    user_poo v3
    
    user_poo v4

--- guard_clase_fullname.rb
+++
@@ -1,7 +0,0 @@
-def longname
-  name = fullname
-  return name unless organization_id
-  organization = Organization.lookup(id: organization_id) 
-  return name += " (#{organization.name})" if organization
-  name
-end    
