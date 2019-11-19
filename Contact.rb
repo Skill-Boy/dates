ad81c0ab06240773f2f4af1064b4a424341801a6
trainee1 <erickakegil@outlook.es>
2019-11-19 13:55:29 -0600

    Contact poo

---
+++ Contact.rb
@@ -0,0 +1,29 @@
+class Contact
+  attr_accessor :contacts 
+  
+  def initialize(contacts)
+    @contacts = contacts
+  end
+
+  @contacts = [{age: 20, name: "Jose",   last_name: "Lopez",   gender: :masculino},
+               {age: 21, name: "Oscar",  last_name: "Perez",   gender: :masculino},
+               {age: 22, name: "Martha", last_name: "Sanchez", gender: :femenino}]  
+    
+  def all_contacts
+    @contacts    
+  end
+
+  def find_by(key, value)
+    contacts.each do |hashes|
+      return hashes
+    end
+  end  
+          
+end
+
+contacts = [{age: 20, name: "Jose", last_name: "Lopez", gender: :masculino},
+    {age: 21, name: "Oscar",  last_name: "Perez",   gender: :masculino},
+    {age: 22, name: "Martha", last_name: "Sanchez", gender: :femenino}]  
+contact = Contact.new(contacts)
+contact.all_contacts
+contact.find_by("last_name", "Lopez")
