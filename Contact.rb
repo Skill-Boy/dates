7b400da7149f59218b0307bc0bc2512e5171d7b0
trainee1 <erickakegil@outlook.es>
2019-11-19 17:01:14 -0600

    Contact poo v7

--- Contact.rb
+++ Contact.rb
@@ -1,17 +1,16 @@
 class Contact
-  def all_contacts
-    contacts = [{age: 20, name: "Jose", last_name: "Lopez", gender: :masculino},
-      {age: 21, name: "Oscar", last_name: "Perez", gender: :masculino},
-      {age: 22, name: "Martha", last_name: "Sanchez", gender: :femenino}]      
+  def self.all
+    [{age: 20, name: "Jose", last_name: "Lopez", gender: :masculino},
+    {age: 21, name: "Oscar", last_name: "Perez", gender: :masculino},
+    {age: 22, name: "Martha", last_name: "Sanchez", gender: :femenino}]      
   end
 
-  def find_by(key, value)
-    all_contacts.each do |names|
+  def self.find_by(key, value)
+    all.each do |names|
       return names if names[key.to_sym] == value
     end
   end         
 end
 
-contact = Contact.new
-contact.all_contacts
-contact.find_by("last_name", "Lopez")
+Contact.all
+Contact.find_by("last_name", "Lopez")
